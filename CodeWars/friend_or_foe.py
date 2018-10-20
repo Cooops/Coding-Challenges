733ffaad27d50510c56ce1811dc07affb07a052b
Cooper Ribb <cooperlimond@gmail.com>
2018-10-20 09:42:13 -0500

    Create friend_or_foe.py

---
+++ CodeWars/friend_or_foe.py
@@ -0,0 +1,11 @@
+# MY SOLUTION
+def friend_or_foe(x):
+    """ (list) -> list
+
+    Takes in a list of names and returns those with exactly 4 characters.
+    print(friend_or_foe(["Ryan", "Kieran", "Mark",])) => ["Ryan", "Mark"]
+    """
+    return [i for i in x if len(i) == 4]
+
+# TOP SOLUTION
+Same thing :)
