1ff741437d234e22192c1e2e445775d0b732f60e
Cooper Ribb <cooperlimond@gmail.com>
2018-10-21 17:33:37 -0500

    Update regexp_replace.sql

--- CodeWars/sql/regexp_replace.sql
+++ CodeWars/sql/regexp_replace.sql
@@ -1,1 +1,27 @@
+# https://www.codewars.com/kata/sql-regex-replace/sql
+"""
+You are given a table named repositories, format as below:
 
+repositories table schema
+
+project
+commits
+contributors
+address
+The table shows project names of major cryptocurrencies, their numbers of commits and contributors and also a random donation address ( not linked in any way :) ).
+
+Your job is to remove all numbers in the address column and replace with '!', then return a table in the following format:
+
+output table schema
+
+project
+commits
+contributors
+address
+Case should be maintained.
+"""
+
+# MY SOLUTION (PostgreSQL is 
+UPDATE repositories
+SET address = REGEXP_REPLACE(address, '[[:digit:]]', '!', 'g')
+RETURNING *
