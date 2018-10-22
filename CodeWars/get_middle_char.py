11961f44ade69453806121e67e2cea59cc2614fd
Cooper Ribb <cooperlimond@gmail.com>
2018-10-22 02:47:22 -0500

    Create get_middle_char.py

---
+++ CodeWars/get_middle_char.py
@@ -0,0 +1,33 @@
+# https://www.codewars.com/kata/get-the-middle-character
+
+"""
+You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+#Input
+
+A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+#Output
+
+The middle character(s) of the word represented as a string.
+"""
+from statistics import median
+
+# MY SOLUTION
+def get_middle(s):
+    middle = int(len(s)/2)
+    # return s[middle] if len(s)%2!=0 else f'{s[middle-1]}{s[middle]}'  # python 3.6 not available, so we format the old way.
+    return s[middle] if len(s)%2!=0 else '{}{}'.format(s[middle-1], s[middle])
+
+# TOP SOLUTION
+def get_middle(s):
+   return s[(len(s)-1)/2:len(s)/2+1]
