f12473e76f11a12ec6c666b3bae48effb9d1d73c
Cooper Ribb <cooperlimond@gmail.com>
2018-10-22 12:45:11 -0500

    Update engineering_interview_question_2.py

--- Coinbase/engineering_interview_question_2.py
+++ Coinbase/engineering_interview_question_2.py
@@ -25,19 +25,19 @@ def minNum(A, K, P):
         return 'she has already reached asha'
     else:
         for i in range(1, 1000000):
-            print(f'Asha: {A*i+2} problems')
+            print(f'Asha: {A*i+P} problems')
             print(f'Kelly: {K*i} problems')
-            print(f'Diff: {A*i+2-K*i} problems')
+            print(f'Diff: {A*i+P-K*i} problems')
             print()
             time.sleep(1)
             # if we are on the second loop and the delta hasn't changed, then the delta will never change.
-            if i == 2 and A*i+2-K*i == P:
-                return 'she will never reach Asha (diff same)'
+            if i == 2 and A*i+P-K*i == P:
+                return 'she will never reach Asha (diff same) (-1)'
             # otherwise, if the delta is growing, then kelly will never reach asha.
-            elif i == 2 and A*i+2-K*i > P:
+            elif i == 2 and A*i+P-K*i > P:
                 return 'she will never reach Asha (diff growing)'
-            # else when kelly passes asha, return the number of days it took her
-            elif K*i > A*i+2:
+            # otherwise, when kelly passes asha return the number of days it took her
+            elif K*i > A*i+P:
                 return f'it would take her {i} days'
 
 print(minNum(A=10, K=11, P=2))
