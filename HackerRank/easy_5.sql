4e6700c48847441e34b0b71aa4ca4d7d54f23a29
Cooper Ribb <cooperlimond@gmail.com>
2018-10-22 12:46:03 -0500

    Delete easy_5.sql

--- HackerRank/easy_5.sql
+++
@@ -1,3 +0,0 @@
-# https://www.hackerrank.com/challenges/weather-observation-station-5
-
-select min(length(lenName)), max(length(lenName)) from (select length(name) from test order by name asc) as lenName;
