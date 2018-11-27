da00d12b685c30e11936cbfe05bc7eb78e396a86
Cooper Ribb <cooperlimond@gmail.com>
2018-11-27 12:45:19 -0600

    Update vowels_at_both_ends.sql

--- HackerRank/vowels_at_both_ends.sql
+++ HackerRank/vowels_at_both_ends.sql
@@ -6,5 +6,5 @@ Your result cannot contain duplicates.
 """
 SELECT DISTINCT city
 FROM station 
-WHERE LEFT(city, 1) in ('a','e','i','o','u') -- pops out the first element in the string (city) and checks if it is in our list of vowels
-AND RIGHT(city, 1) in ('a','e','i','o','u'); -- same thing for the last element in the string
+WHERE LEFT(city, 1) IN ('a','e','i','o','u') -- pops out the first element in the string (city) and checks if it is in our list of vowels
+AND RIGHT(city, 1) IN ('a','e','i','o','u'); -- same thing for the last element in the string
